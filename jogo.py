5074507b87dd7f53821c99de482ec956b415218d
Thulio <thulio.alves@gmail.com>
2020-07-17 10:04:37 -0300

    Adicionando tentativas, pontos e gerando número random

--- jogo.py
+++ jogo.py
@@ -10,3 +10,9 @@ def jogo():
     print("Escolha o nível de dificuldade")
 
     nivel = int(input("[1]Fácil [2]Médio [3]Difícil: )")
+    if nivel == 1:
+        total_de_tentativas = 15
+    elif nivel == 2:
+        total_de_tentativas = 10
+    else:
+        total_de_tentativas = 5
